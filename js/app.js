c2ad765d625919df77e3fef7852f697ecd1da30f
lautajam <lautajamdev@gmail.com>
2022-09-08 02:22:39 -0300

    i made print in display of character data

--- js/app.js
+++ js/app.js
@@ -1,20 +1,45 @@
 
 // console.log("I'm linked");
 
+const cardCharacter = document.getElementById("cardCharacter");
+const templateCharacter = document.getElementById("templateCharacter");
+
 const urlCharters = "https://rickandmortyapi.com/api/character/";
 
-fetch(urlCharters + parseInt(prompt("Select an ID:")))
+const fragment = document.createDocumentFragment();
+
+fetch(urlCharters + parseInt(prompt("Select ID: ")))
     .then(res => res.json())
     // .then(data => console.log(data))
     .then(data => {
-    console.log(`
-    The charter selected:
-    Photo: ${data.image}
-    ID: ${data.id}
-    Name: ${data.name}
-    Specie: ${data.species}
-    Status: ${data.status}
-    Location: ${data.location.name}
-    Origin: ${data.origin.name}
-    `)});
+        console.log(`
+            The charter selected:
+            Photo: ${data.image}
+            ID: ${data.id}
+            Name: ${data.name}
+            Specie: ${data.species}
+            Status: ${data.status}
+            Location: ${data.location.name}
+            Origin: ${data.origin.name}
+            `)
+        printCharacter(data);
+    });
 
+
+const printCharacter = (data) => {
+
+    cardCharacter.textContent = "";
+
+    const clone = templateCharacter.content.cloneNode(true);
+    clone.querySelector("#photo").setAttribute("src", data.image);
+    clone.querySelector("#name").textContent = data.name;
+    clone.querySelector("#specie").textContent = "Specie: " + data.species;
+    clone.querySelector("#status").textContent = "Status: " + data.status;
+    clone.querySelector("#location").textContent = "Location: " + data.location.name;
+    clone.querySelector("#origin").textContent = "Origin: " + data.origin.name;
+
+    fragment.appendChild(clone);
+    
+    cardCharacter.appendChild(fragment);
+
+}
