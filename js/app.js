99f754c21325d1a14fb46afb35daf8f47541d79f
lautajam <lautajamdev@gmail.com>
2022-09-08 01:15:04 -0300

    now I consume the API and show the data in console and I have made a mini example card showing rick's static data

--- js/app.js
+++ js/app.js
@@ -1,2 +1,20 @@
 
-console.log("I'm linked");
+// console.log("I'm linked");
+
+const urlCharters = "https://rickandmortyapi.com/api/character/";
+
+fetch(urlCharters + parseInt(prompt("Select an ID:")))
+    .then(res => res.json())
+    // .then(data => console.log(data))
+    .then(data => {
+    console.log(`
+    The charter selected:
+    Photo: ${data.image}
+    ID: ${data.id}
+    Name: ${data.name}
+    Specie: ${data.species}
+    Status: ${data.status}
+    Location: ${data.location.name}
+    Origin: ${data.origin.name}
+    `)});
+
